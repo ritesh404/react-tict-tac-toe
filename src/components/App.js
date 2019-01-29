ea7ea7e20225e1e3c5eb46bb029d9495f9062dd2
breezyboa <blitzritz@gmail.com>
2019-01-29 18:03:31 +0530

    Add basic user action

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,23 @@ import Header from "./Header";
 import GameBoard from "./GameBoard";
 import Footer from "./Footer";
 import { withState, compose } from "recompose";
+import { defaultBoard } from "../lib/constants";
 
-const enhance = compose(withState("currentPlayer", "setCurrentPlayer", "X"));
+const enhance = compose(
+  withState("currentPlayer", "setCurrentPlayer", "X"),
+  withState("board", "setBoard", defaultBoard)
+);
 
-const App = ({ currentPlayer, setCurrentPlayer }) => (
+// TODO: Get rid of prop drilling
+const App = ({ currentPlayer, setCurrentPlayer, board, setBoard }) => (
   <div className="App">
     <Header currentPlayer={currentPlayer} />
-    <GameBoard />
+    <GameBoard
+      board={board}
+      setBoard={setBoard}
+      currentPlayer={currentPlayer}
+      setCurrentPlayer={setCurrentPlayer}
+    />
     <Footer />
   </div>
 );
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,17 +4,48 @@ import { defaultBoard } from "../lib/constants";
 
 const { Maybe, caseOf } = K;
 const { Nothing, Just } = Maybe;
-const BoardCell = ({ value = "" }) => <div className="board-cell">{value}</div>;
 
-const GameBoard = ({ board = defaultBoard }) => (
+const BoardCell = ({ value = "", onClick = K.id }) => (
+  <div onClick={onClick} className="board-cell">
+    {value}
+  </div>
+);
+
+const updatePosition = (index, value, updateFn, board) => {
+  const newBoard = [...board];
+  newBoard[index] = Just(value);
+  updateFn(newBoard);
+};
+
+const togglePlayer = currentPlayer => (currentPlayer === "X" ? "O" : "X");
+
+const GameBoard = ({
+  board = defaultBoard,
+  setBoard,
+  currentPlayer,
+  setCurrentPlayer
+}) => (
   <div className="game-section">
     <div className="game-board">
-      {K.fmap(
-        caseOf({
-          Just: value => <BoardCell value={value} />,
-          Nothing: _ => <BoardCell value={"x"} />
-        }),
-        board
+      {board.map((cell, index) =>
+        caseOf(
+          {
+            Just: value => <BoardCell value={value} />,
+            Nothing: _ => (
+              <BoardCell
+                onClick={_ => {
+                  updatePosition(index, currentPlayer, setBoard, board);
+                  K.compose(
+                    setCurrentPlayer,
+                    togglePlayer
+                  )(currentPlayer);
+                }}
+                value={"x"}
+              />
+            )
+          },
+          cell
+        )
       )}
     </div>
   </div>
